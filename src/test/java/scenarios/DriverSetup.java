0788ebbcb42535fff6c69d880fe4dcd74228197f
dashanchik <https://github.com/Dashanchik>
2019-03-21 02:10:18 +0300

    Homework1 started

---
+++ src/test/java/scenarios/DriverSetup.java
@@ -0,0 +1,26 @@
+package scenarios;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverSetup {
+    protected AndroidDriver driver;
+
+    protected void prepareAndroidNative() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("adbExecTimeout", "50000");
+
+        File appDir = new File ("C:\\Users\\musin\\Downloads");
+        File app = new File (appDir, "ContactManager.apk");
+
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
